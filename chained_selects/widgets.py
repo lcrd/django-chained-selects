b83f45736a08b38b3b7453b16baadfad54b4be82
Moreno <moreno.pinheiro@gmail.com>
2013-10-04 12:44:50 -0300

    PEP8

--- chained_selects/widgets.py
+++ chained_selects/widgets.py
@@ -14,7 +14,10 @@ if django.VERSION >= (1, 2, 0) and getattr(
     USE_DJANGO_JQUERY = True
 else:
     USE_DJANGO_JQUERY = False
-    JQUERY_URL = getattr(settings, 'JQUERY_URL', 'http://ajax.googleapis.com/ajax/libs/jquery/1.3.2/jquery.min.js')
+    JQUERY_URL = getattr(
+        settings,
+        'JQUERY_URL',
+        'http://ajax.googleapis.com/ajax/libs/jquery/1.3.2/jquery.min.js')
 
 URL_PREFIX = getattr(settings, "CHAINED_SELECTS_URL_PREFIX", "")
 
@@ -31,19 +34,26 @@ class ChainedSelectWidget(Select):
             )
         js += (static('chained_selects/delayed_fill.js'), )
 
-    def __init__(self, parent_name, app_name, model_name, method_name, *args, **kwargs):
+    def __init__(self, parent_name, app_name, model_name, method_name,
+                                                        *args, **kwargs):
         self.datas = {
             'data-parent-id': 'id_%s' % parent_name,
-            'data-url': '/%(prefix)s/%(app_name)s/%(model_name)s/%(method_name)s/' % {
-                'prefix': kwargs.get('url_prefix', 'chained'),
-                'app_name': app_name,
-                'model_name': model_name,
-                'method_name': method_name, },
-            'data-empty-label': '---------', }
+            'data-url': (
+                '/%(prefix)s'
+                '/%(app_name)s'
+                '/%(model_name)s'
+                '/%(method_name)s/') % {
+                    'prefix': kwargs.get('url_prefix', 'chained'),
+                    'app_name': app_name,
+                    'model_name': model_name,
+                    'method_name': method_name
+                },
+            'data-empty-label': '---------'}
         super(ChainedSelectWidget, self).__init__(*args, **kwargs)
 
     def render(self, name, value, attrs=None, choices=()):
-        if value is None: value = ''
+        if value is None:
+            value = ''
         attrs.update(dict(self.datas, **{'class': 'chained'}))
         final_attrs = self.build_attrs(attrs, name=name)
         output = [u'<select%s>' % flatatt(final_attrs)]
