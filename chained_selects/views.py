de6b32c55618ebf223d526bf7137f5df45f963cc
Moreno Cunha <moreno.pinheiro@gmail.com>
2015-11-19 18:20:33 -0200

    Fix RemovedInDjango19Warning: django.db.models.get_model is deprecated.

--- chained_selects/views.py
+++ chained_selects/views.py
@@ -2,12 +2,12 @@
 import json
 
 from django.utils import six
+from django.apps import apps
 from django.http import HttpResponse
-from django.db.models import get_model
 
 
 def filterchain_all(request, **kwargs):
-    Model = get_model(kwargs['app_name'], kwargs['model_name'])
+    Model = apps.get_model(kwargs['app_name'], kwargs['model_name'])
     obj = Model.objects.get(pk=kwargs['pk'])
     qs = getattr(obj, kwargs['method_name'])()
 
