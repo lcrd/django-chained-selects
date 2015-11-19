b14742b23a4cc2fd8ca6479fc6db5f35759f2a95
Moreno Cunha <moreno.pinheiro@gmail.com>
2015-11-19 18:08:24 -0200

    Porting to Python 3

--- chained_selects/views.py
+++ chained_selects/views.py
@@ -1,18 +1,20 @@
 # -*- coding: utf-8 -*-
-
 import json
 
-from django.db.models import get_model
+from django.utils import six
 from django.http import HttpResponse
+from django.db.models import get_model
 
 
-def filterchain_all(request, app_name, model_name, method_name, pk):
-    Model = get_model(app_name, model_name)
-    obj = Model.objects.get(pk=pk)
-    qs = getattr(obj, method_name)()
-    results = list(qs)
-    final = []
-    for item in results:
-        final.append({'value': item.pk, 'display': unicode(item)})
-    json_response = json.dumps(final)
-    return HttpResponse(json_response, content_type='application/json')
+def filterchain_all(request, **kwargs):
+    Model = get_model(kwargs['app_name'], kwargs['model_name'])
+    obj = Model.objects.get(pk=kwargs['pk'])
+    qs = getattr(obj, kwargs['method_name'])()
+
+    final = [
+        {
+            'value': item.pk,
+            'display': six.text_type(item)
+        } for item in qs]
+
+    return HttpResponse(json.dumps(final), content_type='application/json')
